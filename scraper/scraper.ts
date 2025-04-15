a55a293986ff0c05d7c94f017846e87d28d5cbc0
kishan-kareliya <kareliyakishan007@gmail.com>
2025-04-15 12:05:39 +0530

    setup for production

--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -56,12 +56,7 @@ async function startBot(
 
   const browser = await puppeteer.launch({
     headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
--- server.ts
+++ server.ts
@@ -8,11 +8,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/jobs", async (req: Request, res: Response): Promise<any> => {
-  const startTime = Date.now();
-
-  const beforeMemoryUsage = process.memoryUsage();
-  const beforeCPUUsage = process.cpuUsage();
-
   const jobRole = req.query.role as string;
   const jobLocation = req.query.location as string;
   const limit = Number(req.query.limit);
@@ -24,35 +19,7 @@ app.get("/jobs", async (req: Request, res: Response): Promise<any> => {
   try {
     console.log(`Finding jobs for ${jobRole} in location ${jobLocation}`);
     const jobs = await startBot(jobRole, jobLocation, limit);
-
-    // Track memory usage after request
-    const afterMemoryUsage = process.memoryUsage();
-    const afterCPUUsage = process.cpuUsage();
-
-    const elapsedTime = Date.now() - startTime;
-
-    const memoryUsage = {
-      rss: afterMemoryUsage.rss - beforeMemoryUsage.rss,
-      heapTotal: afterMemoryUsage.heapTotal - beforeMemoryUsage.heapTotal,
-      heapUsed: afterMemoryUsage.heapUsed - beforeMemoryUsage.heapUsed,
-    };
-
-    const cpuUsage = {
-      user: (afterCPUUsage.user - beforeCPUUsage.user) / 1000,
-      system: (afterCPUUsage.system - beforeCPUUsage.system) / 1000,
-    };
-
-    console.log("Memory Used: \n");
-    console.log(memoryUsage);
-    console.log("Cpu Usage\n");
-    console.log(cpuUsage);
-
-    return res.json({
-      jobs,
-      memoryUsage,
-      cpuUsage,
-      timeTakenMs: elapsedTime,
-    });
+    return res.json(jobs);
   } catch (error) {
     console.error("Error scraping jobs:", error);
     return res.status(500).json({ error: "Something went wrong" });
